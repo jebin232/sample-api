0bfc38578d39f5539b06e6758c93f5906ba23e1b
Jebin_3 <90751436+jebin232@users.noreply.github.com>
2023-10-17 06:02:32 -1200

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
-from rembg import remove
 from PIL import Image
 from io import BytesIO
+from rembg import remove
+import base64
 
 app = FastAPI()
 
 @app.post("/remove_background/")
 async def remove_background(file: UploadFile):
-    # Check if the uploaded file is an image
-    if file.content_type.startswith('image'):
-        # Read the uploaded image
-        image_bytes = await file.read()
+    try:
+        # Read the uploaded image and remove the background using rembg
+        with BytesIO() as buffer:
+            image = Image.open(file.file)
+            output = remove(image)
 
-        # Use rembg to remove the background
-        output_bytes = remove(image_bytes)
+            # Save the result to a buffer
+            output.save(buffer, format="PNG")
+            buffer.seek(0)
 
-        # Open the processed image with Pillow
-        image = Image.open(BytesIO(output_bytes))
+            # Encode the image as base64
+            result_base64 = base64.b64encode(buffer.read()).decode()
 
-        # Save the processed image to a temporary file
-        output_path = "output.png"
-        image.save(output_path, "PNG")
-
-        # Return the processed image
-        return FileResponse(output_path, media_type="image/png")
-
-    return {"error": "Uploaded file is not an image."}
+        return {"image_base64": result_base64}
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
